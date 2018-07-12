902101fe1c33d8b1456b349f7fffcd327e659a64
core-santander <pugipedro@gmail.com>
2018-07-12 17:35:18 +0200

    pr v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TextoComponent } from './texto/texto.component';
+import { GlosarioDirective } from './directivas/glosario.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextoComponent,
+    GlosarioDirective
   ],
   imports: [
     BrowserModule
